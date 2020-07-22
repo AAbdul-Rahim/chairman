b0da7709554a48a00bb56c569222aa6367d5fbfe
abdul-rahim <alhassanabdulrahim25@gmail.com>
2020-07-22 00:25:01 +0000

    all

---
+++ buyer/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=0.1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+
+        <!-- css files -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+
+    <title>BUYER LOGIN</title>
+</head>
+<body>
+    <div class="site_container">
+        <div class="login_container">
+            <div class="login_box">
+                <form action="" method="POST" class="form1">
+                    <h6 id="form1_fh">buyer's login form</h6>
+                    <div>
+                        <i class="fa fa-users form1_fa"></i>
+                        <input type="text" id="form1_input" placeholder="Buyer's Name">
+                    </div>
+                    <div>
+                        <i class="fa fa-lock form1_fa"></i>
+                        <input type="password" id="form1_input" placeholder="Password">
+                    </div>
+                    <div>
+                        <input type="submit" value="login" id="form1_submit">
+                    </div>
+                    <a href="../index.php">
+                        <i class="fa fa-angle-double-left"></i>
+                        go back
+                    </a>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+        <!-- footer -->
+        <?php include_once '../footer.php' ; ?>
+        
+
+        <!-- js files -->
+        <script src="../js/jquery-3.4.1.slim.min.js" charset="utf-8"></script>
+        <script src="../js/popper.min.js" charset="utf-8"></script>
+        <script src="../js/bootstrap.min.js" charset="utf-8"></script>
+        <script src="../js/jquery-3.4.1.min.js" charset="utf-8"></script>
+        <script src="../js/main.js" charset="utf-8"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,142 +10,54 @@
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 
-    <title>ECAS | HOME</title>
+    <title>HOME</title>
   </head>
   <body>
-
-    <div class="all_container">
-
-  			<header>
-          <div class="top_header"> 
-            <!-- site logo -->
-            <div class="logo">
-              <a href="index.php">ecas</a>
-            </div>
-
-            <!-- toggle -->
-            <div class="toggle-btn">
-              <i class="fa fa-bars toggle-bars" aria-hidden="true"></i>
-            </div>
-
-            <!-- nav links -->
-            <nav class="menus">
-              <ul>
-
-                <li>
-                  
-                  <a href="">
-                    <i class="fa fa-home"></i>
-                      home
-                  </a>
-                  
-                </li>
-
-                <li> 
-                  <a href="">about us</a>
-                </li>
-
-                <li>
-                  <a href="#contact">contact us</a>
-                </li>
-
-                <li>
-                  <a href="user/login.php">
-                    <i class="fa fa-users"></i>
-                    login
-                  </a>
-                </li>
-
-              </ul>
-            </nav>
+    <div class="site_container">
+      <nav class="navbar navbar-expand-lg sticky-top navbar_bg">
+        <a class="navbar-brand logo" href="#">Navbar</a>
+        <button class="navbar-toggler toggle_btn" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars"></i>
+        </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav  ml-auto navbar_ul">
+              <li class="nav-item active">
+                <a class="nav-link navbar_a" href="#">home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link navbar_a" href="#">about us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link navbar_a" href="#">contact us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link navbar_a" href="seller/login.php">seller</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link navbar_a" href="buyer/login.php">buyer</a>
+              </li>
+              <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle navbar_a" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown link
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li> -->
+            </ul>
           </div>
-        </header>
-
-        <!-- slider -->
-        <div class="slider">
-          <div class="details">
-            <div class="details-top">
-              <h5>exams cell automation system</h5>
-            </div>
-            <div class="details-body">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Odio vitae sed molestias adipisci officiis eveniet natus 
-                cupiditate saepe, in corporis, impedit sapiente ratione ab. 
-                Repellat officiis totam eos omnis alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              </p>
-            </div>
-            
-            <a href="">read more...</a>
-          
-          </div>
-        </div>
-
-        <section id="home" class="home"></section>
-        <section id="about" class="about"></section>
-        <section id="contact" class="contact">
-          <h1>contact us</h1>
-          <form action="">
-            <div>
-              <p>full name</p>
-              <input type="text" placeholder="Enter Your Name">
-            </div>
-            <div>
-              <p>title</p>
-              <input type="text" placeholder="Enter The Title">
-            
-              <p>your message</p>
-              <textarea rows="4"></textarea>
-            </div>
-            <input type="submit" value="send message">
-          </form>
-          <div class="other-contact">
-
-            <div>
-              <h5>email us</h5>
-                <p>infoecas@gmail.com</p>
-                <p>infoecas@facebook.com</p>
-            </div>
-
-            <div>
-              <h5>call us</h5>
-                <p>+23324748759</p>
-                <p>+233201063783</p>
-            </div>
-              
-            
-          </div>
-        </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      </nav>
     </div>
-    <?php include_once 'footer.php' ?>
+
+  
+
+
+
+        <!-- footer -->
+        <?php include_once 'footer.php' ; ?>
+        
 
         <!-- js files -->
         <script src="js/jquery-3.4.1.slim.min.js" charset="utf-8"></script>
