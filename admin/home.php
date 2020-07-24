78cdb8483148518689e363ba11b70a36f2a2d9e0
MahmudSuleman <mudisheikh@outlook.com>
2020-07-24 11:52:27 +0000

    separate header in the admin section

--- admin/home.php
+++ admin/home.php
@@ -1,46 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=0.1">
-        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+<?php
+$title = "HOME";
+include_once "./_header.php";
+?>
 
-        <!-- css files -->
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" href="../fontawesome/css/all.min.css">
-        <link rel="stylesheet" href="../css/style.css">
-
-        <title>ADMIN'S HOME</title>
-</head>
-<body>
-    <div class="site_container">
-        <nav class="navbar navbar-expand-lg sticky-top navbar_bg">
-            <a class="navbar-brand logo" href="#">Navbar</a>
-            <button class="navbar-toggler toggle_btn" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars"></i>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav  ml-auto navbar_ul">
-                <li class="nav-item active">
-                    <a class="nav-link navbar_a active" href="#home">admin's home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar_a" href="users.php">add user</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar_a" href="users_records.php">records</a>
-                </li>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar_a" href="signout.php">signout</a>
-                </li>
-            </ul>
-            </div>
-        </nav>
-
-        <section id="home" class="home">
-        </section>
+<section id="home" class="home">
+</section>
 
 
 
@@ -51,21 +15,22 @@
 
 
 
-    </div>
+</div>
 
 
 
 
 
-        <!-- footer -->
-        <?php include_once '../footer.php' ; ?>
-        
+<!-- footer -->
+<?php include_once '../footer.php'; ?>
 
-        <!-- js files -->
-        <script src="../js/jquery-3.4.1.slim.min.js" charset="utf-8"></script>
-        <script src="../js/popper.min.js" charset="utf-8"></script>
-        <script src="../js/bootstrap.min.js" charset="utf-8"></script>
-        <script src="../js/jquery-3.4.1.min.js" charset="utf-8"></script>
-        <script src="../js/main.js" charset="utf-8"></script>
-    </body>
+
+<!-- js files -->
+<script src="../js/jquery-3.4.1.slim.min.js" charset="utf-8"></script>
+<script src="../js/popper.min.js" charset="utf-8"></script>
+<script src="../js/bootstrap.min.js" charset="utf-8"></script>
+<script src="../js/jquery-3.4.1.min.js" charset="utf-8"></script>
+<script src="../js/main.js" charset="utf-8"></script>
+</body>
+
 </html>
